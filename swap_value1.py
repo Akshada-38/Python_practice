28cae3bfd09986e3484ee61370787a798718712d
Akshada <akshadawagh@123gmail.com>
2026-01-19 11:54:54 +0530

    Swap two variables using a temporary variable

---
+++ swap_value1.py
@@ -0,0 +1,10 @@
+num1 = 10
+num2 = 5
+
+temp = num1      #copy num1 to temp variable
+num1 = num2      #copy num2 to num1
+num2 = temp      #copy temp to num2
+
+print("After swapping num1 is :", num1)
+
+print("After swapping num2 is :", num2)
