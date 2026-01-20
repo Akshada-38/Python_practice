fe2f7c5e33fb92ab07b82b1aa8170250152236cd
Akshada <akshadawagh@123gmail.com>
2026-01-20 12:54:55 +0530

    Find number is possitive,negative or zero Using if_elif statement

--- if_elif.py
+++ if_elif.py
@@ -2,7 +2,7 @@ num = int(input("Enter the number : "))
 if num > 0 :
     print("It's Possitive number.")
 elif num < 0 :
-    print("It's Negative number .")
+    print("It's Negative number.")
 else :
     print("This number is zero")
           
