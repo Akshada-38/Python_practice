2833aadb35a91fa1f99412dbd4a7907ad848a1a8
Akshada <akshadawagh@123gmail.com>
2026-01-21 11:38:17 +0530

    Updates multiple list elements at once using Python list slicing

---
+++ list_slicing.py
@@ -0,0 +1,4 @@
+name = ["Alice", "Sneha", "Bob", "Charlie", "David", "Eve"]
+# Slicing the list to update the third to sixth elements
+name[2:6] = [45, False, 3.4, "W"]
+print("After slicing and updating :", name)
