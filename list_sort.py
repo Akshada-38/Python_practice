4b744c3a0a5351328c5f903cff904ef656e33b77
Akshada <akshadawagh@123gmail.com>
2026-01-22 11:00:29 +0530

    Sorts a list of integers in ascending and descending order using Python’s built-in sort() method.

---
+++ list_sort.py
@@ -0,0 +1,10 @@
+# Define a list of integers
+int_list = [5, 100, 500, 9, 10, 51, 17, 22, 1, 0]
+
+#sort a list of integers in ascending order.
+int_list.sort()
+print("Sorted list in ascending order:", int_list)
+#sort a list of integers in descending order.
+int_list.sort(reverse=True)
+print("Sorted list in descending order:", int_list)
+
