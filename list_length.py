722dafa6102374f378030c674326172787dc17c9
Akshada <akshadawagh@123gmail.com>
2026-01-31 20:42:00 +0530

    Shows how to find the length of a list using len() in python

---
+++ list_length.py
@@ -0,0 +1,4 @@
+data = ["Neha" , True , 12.5 , 2+3j , 100 , "Riye"]
+length_of_data = len(data)
+print("The length of the list 'data' is :", length_of_data)
+name = ["Neha" , "Amit" , "Sakshi" , "Aniket" , "Yash"]
