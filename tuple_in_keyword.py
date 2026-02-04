f3d4e48935aedb59c2c8b1067a995ed17c6c544a
Akshada <akshadawagh@123gmail.com>
2026-02-04 12:35:56 +0530

    Checks whether specific elements exist in a tuple using the 'in' keyword and prints the results.

---
+++ tuple_in_keyword.py
@@ -0,0 +1,7 @@
+# for checking if an item exists in a tuple using 'in' keyword
+name = ("Sakshi", "Ankita", "Riya", "Pooja")
+# Checking if "Riya" exists in the tuple
+item_exists = "Riya" in name    
+print("Is 'Riya' present in the tuple?:", item_exists)
+not_exists = "Neha" in name
+print("Is 'Neha' present in the tuple?:", not_exists)
