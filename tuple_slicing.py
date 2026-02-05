ef69d9d698b3485fa27c2ed0584aa1bbc5eb0ffa
Akshada <akshadawagh@123gmail.com>
2026-02-05 21:25:17 +0530

    Demonstrates tuple slicing using both negative and positive indexing in Python.

---
+++ tuple_slicing.py
@@ -0,0 +1,8 @@
+name = ("Riya", "Pooja", "Sakshi", "Ankita ", "Neha", "Sneha")
+#Slicing the tuple to get the third to fifth elements using negative indexing
+sliced_tuple = name[-4:-1]
+print("The sliced tuple from third to fifth elements is :", sliced_tuple)
+
+#Slicing the tuple to get the first to third elements using positive indexing
+sliced_tuple_positive = name[0:3]   
+print("The sliced tuple from first to third elements is :", sliced_tuple_positive)
