eb91e71e42335d6d562621f7b93b737c03baea18
Akshada <akshadawagh@123gmail.com>
2026-02-07 21:57:08 +0530

    Shows how to find the index position of elements in a tuple using the index() method.

--- tuple_item_index.py
+++ tuple_item_index.py
@@ -1,4 +1,4 @@
 name = ("Riya" , "Shreya" , "Priya" , "Divya" , "Ananya")
 # Find the index of an item in a tuple
-print("Index of 'Priya':", name.index("Priya"))
-print("Index of 'Ananya':", name.index("Ananya"))
+print("Index of 'Priya' :", name.index("Priya"))
+print("Index of 'Ananya' :", name.index("Ananya"))
