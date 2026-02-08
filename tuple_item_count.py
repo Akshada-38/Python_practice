4b58e14fdf27f42ab968cb7b8cfbf97c974f5c3b
Akshada <akshadawagh@123gmail.com>
2026-02-08 21:21:25 +0530

    Finds how many times an element appears in a tuple using count() method.

---
+++ tuple_item_count.py
@@ -0,0 +1,4 @@
+alphabet = ("A" , "B" , "C" , "A" , "D" , "A" , "E")
+# count() method is used to count the number of occurrences of an element in a tuple
+print("Count of 'A' in the tuple :", alphabet.count("A"))
+print("Count of 'B' in the tuple :", alphabet.count("B"))
