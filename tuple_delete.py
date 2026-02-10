8183a099bb2aa3c236c20b31c56b39c0a0c2413d
Akshada <akshadawagh@123gmail.com>
2026-02-10 11:51:02 +0530

    Shows how to delete a tuple using the del keyword in Python.

---
+++ tuple_delete.py
@@ -0,0 +1,5 @@
+name = ("Neha", "Shivam", "Rohit" , "Satyarth", "Shivansh")
+print("The original tuple is : " , name)
+# Deleting a tuple using del keyword
+del name
+print("the tuple (name) is deleted successfully")
