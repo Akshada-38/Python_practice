57d4f9a61ae6bf548c5efa51aa317826fb6aa7ed
Akshada <akshadawagh@123gmail.com>
2026-02-11 14:19:34 +0530

    Demonstrates multiple methods for creating sets in Python using different data sources.

---
+++ set_creation.py
@@ -0,0 +1,16 @@
+# Create set using set() method :
+set_from_list = set([1, 2, 3, 4, 5])
+print("Set created from list:", set_from_list)  
+
+# Create set using curly braces :
+set_with_curly_braces = {10, 20, 30, 40, 50}
+print("Set created with curly braces:", set_with_curly_braces)
+
+# Create set using string :
+set_from_string = set("Hello")
+print("Set created from string:", set_from_string)
+
+# Create empty set :
+empty_set = set()
+print("Empty set:", empty_set)
+
