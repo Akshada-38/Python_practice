f0c849c3d8447aad7e53ad9977e2535564fca4c6
Akshada <akshadawagh@123gmail.com>
2026-02-12 15:00:26 +0530

    Demonstrates duplicate removal in sets and finding length using len().

---
+++ set_length.py
@@ -0,0 +1,4 @@
+name = {"neha"  , "priya" , "sneha" , "priya" , "neha" , "sneha"}
+length_of_set = len(name)
+# The length of the set will be 3 because sets do not allow duplicate values. The unique values in the set are "neha", "priya", and "sneha".
+print("Length of the set(name) is:", length_of_set)
