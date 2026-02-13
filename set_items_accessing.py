6f35b3e6d3a3bce29697c73e391d79ee1d57c27d
Akshada <akshadawagh@123gmail.com>
2026-02-13 10:40:22 +0530

    Demonstrates accessing elements of a set by iterating through it, as sets do not support indexing.

---
+++ set_items_accessing.py
@@ -0,0 +1,7 @@
+data = {"Priyal" , 12.5 , "Shivam" , 123 , "Satyarth" , True , 5 + 3j}
+# Accessing items from the set using a loop
+for item in data:
+    print("The item is:", item)
+print("the set(data) is accessed successfully")
+
+#In set, we canot access items using index because sets are unordered and do not support indexing. However, we can access items using a loop or by checking for the presence of an item in the set.
