7e3fab15e3d85d2dd838c33d7edb27d620560d55
Akshada <akshadawagh@123gmail.com>
2026-03-03 11:13:42 +0530

    Safely retrieves the value of key using get() method.

---
+++ dictionary_keys.py
@@ -0,0 +1,10 @@
+data = { 
+    "name" : "Neha",
+    "age" : 22,
+    "city" : "delhi",
+    "country" : "india"
+}
+
+#Method : get() Method
+access_name = data.get("name")
+print("Name is : " ,access_name)
