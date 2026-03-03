b23c24de3a28829af59dadadc95b4ef77a69e48f
Akshada <akshadawagh@123gmail.com>
2026-03-03 11:26:57 +0530

    Retrieves all values from the dictionary using the values() method.

---
+++ dictionary_values_method.py
@@ -0,0 +1,10 @@
+data = {
+    'name': 'Rita',
+    'age': 25,
+    'city': 'Mumbai',
+    'country': 'India'
+}
+
+# Get the values of the dictionary
+values = data.values()
+print("Values of the dictionary:", values)
