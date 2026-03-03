ecfd0a9e2091cef1f356352f4ceca0095b875b21
Akshada <akshadawagh@123gmail.com>
2026-03-03 11:16:55 +0530

    Retrieves all key–value pairs from the dictionary using the items() method.

---
+++ dictionary_items_method.py
@@ -0,0 +1,11 @@
+data = {
+    'name': 'Alice',
+    'age': 30,
+    'city': 'New York',
+    'country': 'USA'
+}
+
+# Using the items() method to get key-value pairs
+items = data.items()
+# Printing the items
+print("The all items are:", items)
