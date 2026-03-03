f26af9f9cb14dccf63a311f09e304de605cfc8d6
Akshada <akshadawagh@123gmail.com>
2026-03-03 11:21:22 +0530

    Retrieves all keys from the dictionary using the keys() method.

---
+++ dictionary_keys_method.py
@@ -0,0 +1,10 @@
+data = {
+    'name': 'Alice',
+    'age': 30,
+    'city': 'New York',
+    'country': 'USA'
+}
+
+# Using the keys() method to get all keys
+keys = data.keys()
+print("The keys are:", list(keys))
