bdbdfc06744a911d3e8c228899c177dd1a21f805
Akshada <akshadawagh@123gmail.com>
2026-03-21 13:37:05 +0530

    Demonstrates type casting in Python using float(), int(), and str() functions, and prints their values along with their data types.

---
+++ type_casting.py
@@ -0,0 +1,13 @@
+x = float(1)   # x will be 1.0
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+a = str("s1") # x will be 's1'
+b = str(2)    # y will be '2'
+c = str(3.0)  # z will be '3.0'
+
+print(x , "type of x:", type(x))
+print(y , "type of y:", type(y))
+print(z , "type of z:", type(z))
+print(a , "type of a:", type(a))
+print(b , "type of b:", type(b))
+print(c , "type of c:", type(c))
