65cde272efab604ef11875217ddcf33060458f5d
Akshada <akshadawagh@123gmail.com>
2026-04-09 21:45:18 +0530

    Uses a for loop with range() to print numbers from 1 to 10.

---
+++ for_loop_with_range_function.py
@@ -0,0 +1,6 @@
+# use range() funstion in for loop for iterating in range of numbers
+# syntax: for variable in range(start, stop, step):
+
+print("Iterating in range of numbers from 1 to 10:")
+for i in range(1, 11):
+    print(i)
