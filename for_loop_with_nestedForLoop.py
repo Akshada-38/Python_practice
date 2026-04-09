6d59a3a26be040be48c8d354c4e0c2f56cab963b
Akshada <akshadawagh@123gmail.com>
2026-04-09 21:52:57 +0530

    Demonstrates nested for loops to iterate over two ranges and print their combined values.

---
+++ for_loop_with_nestedForLoop.py
@@ -0,0 +1,6 @@
+# nested for loops for using for loop inside another for loop
+
+print("printing i and j values using nested for loops:")
+for i in range(1, 4):
+    for j in range(1, 3):
+        print(f"i: {i}, j: {j}")
