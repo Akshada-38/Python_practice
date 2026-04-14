5e551314542b36eeb7fb39457f30fda8b6d0d8bb
Akshada <akshadawagh@123gmail.com>
2026-04-14 14:26:30 +0530

    Defines an empty function using 'pass' as a placeholder for future implementation.

---
+++ function_pass_statement.py
@@ -0,0 +1,6 @@
+# Function definitions cannot be empty. If you need to create a function placeholder without any code, use the pass statement:
+
+def my_function():
+    pass
+print("This is a function with a pass statement.")
+# The pass statement is a null operation; it does nothing when executed. It is used as a placeholder in situations where syntactically some code is required, but you don't want to execute any code at that point.
