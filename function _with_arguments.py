eb736f72d3cfe66e37b891b9fdcd31c54820f26b
Akshada <akshadawagh@123gmail.com>
2026-04-14 14:32:05 +0530

    Defines a function with a parameter and calls it with different arguments to demonstrate parameter passing.

---
+++ function _with_arguments.py
@@ -0,0 +1,10 @@
+# Information can be passed into functions as arguments.
+# Arguments are specified after the function name, inside the parentheses. You can add as many arguments as you want, just separate them with a comma.
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Charly")
+my_function("Tobias")
+my_function("Linus")
+# You can also send arguments with the key = value syntax. This way the order of the arguments does not matter.
