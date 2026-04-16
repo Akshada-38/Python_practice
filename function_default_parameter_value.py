db9e2e9c623a319ab4efca8ab2b115715cbe5bcb
Akshada <akshadawagh@123gmail.com>
2026-04-16 12:57:25 +0530

    Demonstrates a function with a default parameter value and shows how it works with and without passing an argument.

---
+++ function_default_parameter_value.py
@@ -0,0 +1,8 @@
+# Function with default parameter value
+def my_function(name = "friend"):
+  print("Hello", name)
+
+my_function("Priya")
+my_function("Neha")
+my_function()   # This will use the default value "friend"
+my_function("Riya")
