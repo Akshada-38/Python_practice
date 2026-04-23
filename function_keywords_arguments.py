aa5c9e14bcf0083943fa5ea6b7680e76d058b74c
Akshada <akshadawagh@123gmail.com>
2026-04-23 19:32:34 +0530

    Demonstrates a function with a  send arguments with the key = value syntax.

---
+++ function_keywords_arguments.py
@@ -0,0 +1,7 @@
+# You can send arguments with the key = value syntax.
+
+def my_function(animal, name):
+  print("I have a", animal)
+  print("My", animal + "'s name is", name)
+
+my_function(animal = "dog", name = "Buddy")
