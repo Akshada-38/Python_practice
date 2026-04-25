a061c14fc328ffecb30b404fec6a5fab716e3e3b
Akshada <akshadawagh@123gmail.com>
2026-04-25 22:14:55 +0530

    Demonstrates passing a different data type (list,dict,string,number, etc.) as an argument to a function and iterating through its elements.

---
+++ function_Passing_Different_DataTypes.py
@@ -0,0 +1,10 @@
+#You can send any data type as an argument to a function (string, number, list, dictionary, etc.).
+
+# Sending a list as an argument:
+
+def my_function(fruits):
+  for fruit in fruits:
+    print(fruit)
+
+my_fruits = ["apple", "banana", "cherry"]
+my_function(my_fruits)
