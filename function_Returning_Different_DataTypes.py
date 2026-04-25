64561922245f66f5c4e07556a48fe941d0ee430b
Akshada <akshadawagh@123gmail.com>
2026-04-25 22:27:26 +0530

    Demonstrates returning a different data type (list,tuple,num,str,etc.) from a function and accessing its elements.

---
+++ function_Returning_Different_DataTypes.py
@@ -0,0 +1,11 @@
+# Functions can return any data type, including lists, tuples, dictionaries, and more.
+
+#A function that returns a list:
+
+def my_function():
+  return ["apple", "banana", "cherry"]
+
+fruits = my_function()
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
