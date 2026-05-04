7fdc625dcd31071b3efcd552584ce41ea95ad57f
Akshada <akshadawagh@123gmail.com>
2026-05-04 20:48:33 +0530

    Demonstrates a decorator handling functions with arguments and modifying the returned result.

---
+++ function_Arguments_in_Decorated_Function.py
@@ -0,0 +1,12 @@
+# Functions that require arguments can also be decorated, just make sure you pass the arguments to the wrapper function:
+
+def changecase(func):
+  def myinner(x):
+    return func(x).upper()
+  return myinner
+
+@changecase
+def myfunction(nam):
+  return "Hello " + nam
+
+print(myfunction("John"))
