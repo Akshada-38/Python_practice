00c29ce8804a7ecb08eeec20762ce41854e7ad63
Akshada <akshadawagh@123gmail.com>
2026-05-08 11:44:16 +0530

    Demonstrates accessing a function’s name using the __name__ and __doc__ attribute.

---
+++ function_Preserving_Function_Metadata.py
@@ -0,0 +1,6 @@
+def myfunction():
+  return "Have a great day!"
+
+print(myfunction.__name__)  # Output: myfunction
+#  or
+print(myfunction.__doc__)   # Output: None (since we haven't provided a docstring)
