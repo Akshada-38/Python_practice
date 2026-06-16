311183be216876d18306ef05b3bb619abbf5d694
Akshada <akshadawagh@123gmail.com>
2026-06-16 13:52:38 +0530

    # Demonstrates the difference between list comprehensions and generator expressions for creating sequences of values.

---
+++ function_Generator_Expressions.py
@@ -0,0 +1,11 @@
+# Similar to list comprehensions, you can create generators using generator expressions with parentheses instead of square brackets:
+
+
+# List comprehension - creates a list
+list_comp = [x * x for x in range(5)]
+print(list_comp)
+
+# Generator expression - creates a generator
+gen_exp = (x * x for x in range(5))
+print(gen_exp)
+print(list(gen_exp))
