a22b91b0697af64a640a5d57b466727a441c4592
Akshada <akshadawagh@123gmail.com>
2026-06-16 19:35:34 +0530

    # Demonstrates adding all elements from one list to another list using the extend() method.

---
+++ Array_extend.py
@@ -0,0 +1,8 @@
+# Add the elements of a list (or any iterable), to the end of the current list
+
+fruits = ['apple', 'banana', 'cherry']
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+fruits.extend(cars)
+print(fruits)
