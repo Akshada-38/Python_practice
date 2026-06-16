3d6f5cf304ed986418e8aa791a7635dfcbbbb31d
Akshada <akshadawagh@123gmail.com>
2026-06-16 13:46:23 +0530

    # Demonstrates manual iteration over a generator using the next() function to retrieve values one by one.

---
+++ function_Using_next()_with_Generators.py
@@ -0,0 +1,11 @@
+# You can manually iterate through a generator using the next() function:
+
+def simple_gen():
+  yield "Emil"
+  yield "Tobias"
+  yield "Linus"
+
+gen = simple_gen()
+print(next(gen))
+print(next(gen))
+print(next(gen))
