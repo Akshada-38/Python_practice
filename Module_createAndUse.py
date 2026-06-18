0b3977212bdb7a26a4f340db47702ac679de3108
Akshada <akshadawagh@123gmail.com>
2026-06-18 22:31:54 +0530

    # Demonstrates creating a custom Python module, importing it, and calling a function defined in that module.

---
+++ Module_createAndUse.py
@@ -0,0 +1,11 @@
+# To create a module just save the code you want in a file with the file extension .py:
+
+
+def greeting(name):
+  print("Hello, " + name)
+
+
+#Now we can use the module we just created, by using the import statement:
+import Module_createAndUse
+
+Module_createAndUse.greeting("Jonathan")
