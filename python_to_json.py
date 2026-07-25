3074ec2ba28a827d6f292f2bd96d06175bce7b80
Akshada <akshadawagh@123gmail.com>
2026-07-25 21:44:35 +0530

    # Demonstrates using json.dumps() to convert a Python dictionary into a JSON string.

---
+++ python_to_json.py
@@ -0,0 +1,15 @@
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print("This is the Python object:", x)
+print("This is the JSON string (using json.dumps()):", y)
