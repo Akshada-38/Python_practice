181cb43e46b122c85a49a37e585d025cabba07d0
Akshada <akshadawagh@123gmail.com>
2026-08-24 23:20:14 +0530

    # Demonstrates using the \b metacharacter in a regular expression to find a pattern at a word boundary. # \bain → matches 'ain' at the beginning of a word. # ain\b → matches 'ain' at the end of a word.

---
+++ RegEx_/RegEx_/RegEx_slash-b_SpecialSequences.py
@@ -0,0 +1,37 @@
+# Method 1 = r"\bain"
+
+
+import re
+
+txt = "The rain in Spain"
+
+#Check if "ain" is present at the beginning of a WORD:
+
+x = re.findall(r"\bain", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+
+# Method 1 = r"ain\b"
+
+
+import re
+
+txt = "The rain in Spain"
+
+#Check if "ain" is present at the end of a WORD:
+
+y = re.findall(r"ain\b", txt)
+
+print(y)
+
+if y:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
