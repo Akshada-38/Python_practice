f0adbce6f5fd42f2d99c0af4dbee6def66a833b2
Akshada <akshadawagh@123gmail.com>
2026-09-23 22:36:58 +0530

    # Demonstrates using f-strings with format specifiers to display prices with different decimal formatting.

---
+++ Try-Except_Placeholders&Modifiers.py
@@ -0,0 +1,8 @@
+price = 59
+txt = f"The 1st price is {price} dollars"
+print(txt)
+price = 59
+txt = f"The 2nd price is {price:.2f} dollars"
+print(txt)
+txt = f"The 3rd price is {95:.2f} dollars"
+print(txt)
