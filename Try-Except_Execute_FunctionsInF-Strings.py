74f17864b1fae0fb71c0717d80d5856a5ea97e05
Akshada <akshadawagh@123gmail.com>
2026-09-25 21:57:47 +0530

    # Demonstrates using functions and string methods inside f-strings to format and calculate values directly.

---
+++ Try-Except_Execute_FunctionsInF-Strings.py
@@ -0,0 +1,9 @@
+fruit = "apples"
+txt = f"I love {fruit.upper()}"
+print(txt)
+
+def myconverter(x):
+  return x * 0.3048
+
+txt1 = f"The plane is flying at a {myconverter(30000)} meter altitude"
+print(txt1)
